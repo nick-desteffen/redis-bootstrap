c54c763394326ab73ae22605be80f929f647d806
Nick DeSteffen <nick.desteffen@gmail.com>
2013-07-06 15:24:43 -0500

    Adding build-essential package

--- bootstrap_redis.sh
+++ bootstrap_redis.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
+apt-get -y -m install build-essential
+
 cd /tmp
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
-make
-sudo make install
-sudo mkdir /etc/redis
-sudo mkdir /var/redis
-sudo cp utils/redis_init_script /etc/init.d/redis
-sudo sed -i 's/redis_${REDISPORT}.pid/redis.pid/g' /etc/init.d/redis
-sudo sed -i 's/${REDISPORT}.conf/redis.conf/g' /etc/init.d/redis
 
-sudo cp redis.conf /etc/redis/redis.conf
-sudo sed -i 's/daemonize no/daemonize yes/g' /etc/redis/redis.conf
-sudo update-rc.d redis defaults
-sudo /etc/init.d/redis start
+make
+make install
+
+mkdir /etc/redis
+mkdir /var/redis
+
+cp utils/redis_init_script /etc/init.d/redis
+sed -i 's/redis_${REDISPORT}.pid/redis.pid/g' /etc/init.d/redis
+sed -i 's/${REDISPORT}.conf/redis.conf/g' /etc/init.d/redis
+
+cp redis.conf /etc/redis/redis.conf
+sed -i 's/daemonize no/daemonize yes/g' /etc/redis/redis.conf
+
+update-rc.d redis defaults
+/etc/init.d/redis start
 
 redis-server --version
